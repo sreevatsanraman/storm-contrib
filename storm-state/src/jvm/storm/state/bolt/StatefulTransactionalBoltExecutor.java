a382bcfff2d73756a033370263fd8095691a1f1c
Charles Ruhland <cruhland@klout.com>
2012-07-06 13:12:19 -0700

    Added State generics to the transactional bolt executor.

--- storm-state/src/jvm/storm/state/bolt/StatefulTransactionalBoltExecutor.java
+++ storm-state/src/jvm/storm/state/bolt/StatefulTransactionalBoltExecutor.java
@@ -7,28 +7,29 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.transactional.ICommitter;
 import backtype.storm.transactional.TransactionAttempt;
 import backtype.storm.tuple.Tuple;
-import java.util.Map;
 import storm.state.IPartitionedBackingStore;
 import storm.state.PartitionedState;
 import storm.state.State;
 
-public class StatefulTransactionalBoltExecutor implements ICommitter, IBatchBolt<TransactionAttempt> {
+import java.util.Map;
+
+public class StatefulTransactionalBoltExecutor<T extends State> implements ICommitter, IBatchBolt<TransactionAttempt> {
     public static String STATE_RESOURCE = StatefulTransactionalBoltExecutor.class.getName() + "/state";
     
-    IStatefulTransactionalBolt _delegate;
-    State _state;
+    IStatefulTransactionalBolt<T> _delegate;
+    T _state;
     TransactionAttempt _attempt;
     BatchOutputCollector _collector;
     IPartitionedBackingStore _store;
     
-    public StatefulTransactionalBoltExecutor(IStatefulTransactionalBolt delegate, IPartitionedBackingStore store) {
+    public StatefulTransactionalBoltExecutor(IStatefulTransactionalBolt<T> delegate, IPartitionedBackingStore store) {
         _delegate = delegate;
         _store = store;
     }
 
     @Override
     public void prepare(Map conf, TopologyContext context, BatchOutputCollector collector, TransactionAttempt attempt) {
-        _state = (State) context.getTaskData(STATE_RESOURCE);
+        _state = (T) context.getTaskData(STATE_RESOURCE);
         if(_state == null) {
             _state = PartitionedState.getState(conf, context, _store, _delegate.getStateBuilder(), _delegate.getSerializations());        
             context.setTaskData(STATE_RESOURCE, _state);
